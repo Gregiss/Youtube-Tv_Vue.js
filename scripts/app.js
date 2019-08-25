613936dc6ac397db834bc2db5c822cb177784033
Alexandre Silva <xande1231231@hotmail.com>
2019-08-25 19:34:43 -0300

    Fix

---
+++ scripts/app.js
@@ -0,0 +1,227 @@
+var enter = new Audio("./audio/cross-enter.mp3" );
+var light = new Audio("./audio/same-light.mp3" );
+var toggle = new Audio("./audio/same-toggle.mp3" );
+var block = new Audio("./audio/same-heavy.mp3");
+
+function playToogle(){
+	toggle.play();
+	setTimeout(function(){
+	toggle.pause();
+	toggle.currentTime = 0;
+	}, 100);
+}
+
+function playBlock(){
+	block.play();
+	setTimeout(function(){
+	block.pause();
+	block.currentTime = 0;
+	}, 100);
+}
+
+function playlight(){
+	light.play();
+	setTimeout(function(){
+    light.pause();
+    light.currentTime = 0;
+	}, 100);
+}
+
+function playenter(){
+	enter.play();
+	setTimeout(function(){
+    enter.pause();
+    enter.currentTime = 0;
+	}, 300);
+}
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        youtube: null,
+        youtubeLinks: [
+        {"name" : "search", "icon" : "fas fa-search", "have_icon" : true, "acessou" : false, "dentro": false},
+        {"name" : "home", "icon" : "fas fa-home", "have_icon" : true, "acessou" : true,  "dentro": false}
+        ],
+        logo: "Youtube",
+        youtubeAcess: {"id": 1, "dentro": false},
+        termSearchs: [],
+        videos: [],
+        imVideo: -1,
+        scrollX: 0
+    },
+    mounted(){
+        document.title = this.logo;
+        this.getJson()
+    },
+    methods: {
+        getJson(){
+            fetch("./videos.json")
+            .then(r => r.json())
+            .then(json => {
+            this.videos=json;
+            });
+        },
+        acessar(link){
+            playToogle()
+            for(var i = 0; i < this.youtubeLinks.length;i++){
+                this.youtubeLinks[i].acessou = false
+                this.youtubeLinks[i].dentro = false
+            }
+            const index = this.youtubeLinks.indexOf(link)
+            link.acessou = true
+            this.youtubeLinks[index] = link
+            this.youtubeAcess.id = index
+            localStorage.youtubeLinks = JSON.stringify(this.youtubeLinks)
+        },
+        keyBoard(e){
+            if(e.which == 38){
+                if(!this.youtubeAcess.dentro){
+                    if(this.youtubeAcess.id > 0){
+                        this.youtubeAcess.id--
+                        playToogle()
+                        for(var i = 0; i < this.youtubeLinks.length;i++){
+                            this.youtubeLinks[i].acessou = false
+                        }
+                        this.youtubeLinks[this.youtubeAcess.id].acessou = true
+                    } else{
+                        playBlock()
+                    }
+                }
+            }
+            if(e.which == 40){
+                if(!this.youtubeAcess.dentro){
+                    if(this.youtubeAcess.id < this.youtubeLinks.length - 1){
+                        this.youtubeAcess.id++
+                        playToogle()
+                        for(var i = 0; i < this.youtubeLinks.length;i++){
+                            this.youtubeLinks[i].acessou = false
+                        }
+                        this.youtubeLinks[this.youtubeAcess.id].acessou = true
+                    } else{
+                        playBlock()
+                    }
+                }
+            }
+            if(e.which == 39){
+                if(!this.youtubeAcess.dentro){
+                playenter()
+                for(var i = 0; i < this.youtubeLinks.length;i++){
+                    this.youtubeLinks[i].acessou = false
+                    this.youtubeLinks[i].dentro = false
+                }
+                this.youtubeAcess.dentro = true
+                this.youtubeLinks[this.youtubeAcess.id].acessou = false
+                this.youtubeLinks[this.youtubeAcess.id].dentro = true
+                }
+            }
+            if(e.which == 37){
+                if(!this.youtubeLinks[this.youtubeAcess.id].name == 'search' && this.youtubeLinks[0].dentro){
+                if(imVideo == 0){
+                playlight()
+                for(var i = 0; i < this.youtubeLinks.length;i++){
+                    this.youtubeLinks[i].dentro = false
+                    this.youtubeLinks[i].acessou = false
+                }
+                this.youtubeAcess.dentro = false
+                this.youtubeLinks[this.youtubeAcess.id].acessou = true
+                }
+            }
+            }
+            if(e.which == 13){
+                if(!this.youtubeAcess.dentro){
+                playenter()
+                for(var i = 0; i < this.youtubeLinks.length;i++){
+                    this.youtubeLinks[i].acessou = false
+                    this.youtubeLinks[i].dentro = false
+                }
+                this.youtubeAcess.dentro = true
+                this.youtubeLinks[this.youtubeAcess.id].acessou = false
+                this.youtubeLinks[this.youtubeAcess.id].dentro = true
+                }
+            }
+            if(e.which == 27){
+                this.youtubeAcess.dentro = false
+                this.youtubeLinks[this.youtubeAcess.id].acessou = true
+                this.youtubeLinks[this.youtubeAcess.id].dentro = false
+            }
+            if(this.youtubeLinks[1].dentro){
+                if(e.which == 39){
+                    var max = this.videos.contents.tvBrowseRenderer.content.tvSecondaryNavRenderer.sections[0].tvSecondaryNavSectionRenderer.tabs[0].tabRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents[0].shelfRenderer.content.horizontalListRenderer.items.length
+                    console.log(max)
+                    for(var i = 0; i < this.videos.contents.tvBrowseRenderer.content.tvSecondaryNavRenderer.sections[0].tvSecondaryNavSectionRenderer.tabs[0].tabRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents[0].shelfRenderer.content.horizontalListRenderer.items.length; i++){
+                        this.videos.contents.tvBrowseRenderer.content.tvSecondaryNavRenderer.sections[0].tvSecondaryNavSectionRenderer.tabs[0].tabRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents[0].shelfRenderer.content.horizontalListRenderer.items[i].gridVideoRenderer.hover = false
+                    }
+                    if(this.imVideo < max - 1){
+                        playlight()
+                        this.imVideo++
+                        if(this.imVideo >= 1){
+                            this.scrollX = this.imVideo * 28.7 * -1
+                        }
+                    } else{
+                        playBlock()
+                    }
+                    this.videos.contents.tvBrowseRenderer.content.tvSecondaryNavRenderer.sections[0].tvSecondaryNavSectionRenderer.tabs[0].tabRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents[0].shelfRenderer.content.horizontalListRenderer.items[this.imVideo].gridVideoRenderer.hover = true
+                }
+                if(e.which == 37){
+                    var max = this.videos.contents.tvBrowseRenderer.content.tvSecondaryNavRenderer.sections[0].tvSecondaryNavSectionRenderer.tabs[0].tabRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents[0].shelfRenderer.content.horizontalListRenderer.items.length
+                    for(var i = 0; i < this.videos.contents.tvBrowseRenderer.content.tvSecondaryNavRenderer.sections[0].tvSecondaryNavSectionRenderer.tabs[0].tabRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents[0].shelfRenderer.content.horizontalListRenderer.items.length; i++){
+                        this.videos.contents.tvBrowseRenderer.content.tvSecondaryNavRenderer.sections[0].tvSecondaryNavSectionRenderer.tabs[0].tabRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents[0].shelfRenderer.content.horizontalListRenderer.items[i].gridVideoRenderer.hover = false
+                    }
+                    if(this.imVideo > 0){
+                        playlight()
+                        this.imVideo--
+                        this.scrollX = this.imVideo * 28.7 * -1
+                    } else{
+                        playBlock()
+                    }
+                    this.videos.contents.tvBrowseRenderer.content.tvSecondaryNavRenderer.sections[0].tvSecondaryNavSectionRenderer.tabs[0].tabRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents[0].shelfRenderer.content.horizontalListRenderer.items[this.imVideo].gridVideoRenderer.hover = true
+                }
+                if(e.which == 27){
+                    playlight()
+                    for(var i = 0; i < this.youtubeLinks.length;i++){
+                        this.youtubeLinks[i].dentro = false
+                        this.youtubeLinks[i].acessou = false
+                    }
+                    this.youtubeAcess.dentro = false
+                    this.youtubeLinks[this.youtubeAcess.id].acessou = true
+                    for(var i = 0; i < this.videos.contents.tvBrowseRenderer.content.tvSecondaryNavRenderer.sections[0].tvSecondaryNavSectionRenderer.tabs[0].tabRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents[0].shelfRenderer.content.horizontalListRenderer.items.length; i++){
+                        this.videos.contents.tvBrowseRenderer.content.tvSecondaryNavRenderer.sections[0].tvSecondaryNavSectionRenderer.tabs[0].tabRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents[0].shelfRenderer.content.horizontalListRenderer.items[i].gridVideoRenderer.hover = false
+                    }
+                    this.imVideo = -1
+                    this.scrollX = 0
+                }
+            }
+        },
+        hoverOver(video){
+            const index = this.videos.contents.tvBrowseRenderer.content.tvSecondaryNavRenderer.sections[0].tvSecondaryNavSectionRenderer.tabs[0].tabRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents[0].shelfRenderer.content.horizontalListRenderer.items.indexOf(video)
+            for(var i = 0; i < this.videos.contents.tvBrowseRenderer.content.tvSecondaryNavRenderer.sections[0].tvSecondaryNavSectionRenderer.tabs[0].tabRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents[0].shelfRenderer.content.horizontalListRenderer.items.length; i++){
+                this.videos.contents.tvBrowseRenderer.content.tvSecondaryNavRenderer.sections[0].tvSecondaryNavSectionRenderer.tabs[0].tabRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents[0].shelfRenderer.content.horizontalListRenderer.items[i].gridVideoRenderer.hover = false
+            }
+            this.imVideo = index
+            this.srollX = index * 27 * -1
+            this.videos.contents.tvBrowseRenderer.content.tvSecondaryNavRenderer.sections[0].tvSecondaryNavSectionRenderer.tabs[0].tabRenderer.content.tvSurfaceContentRenderer.content.sectionListRenderer.contents[0].shelfRenderer.content.horizontalListRenderer.items[index].gridVideoRenderer.hover = true
+        },
+        search(e){
+            var charTyped = String.fromCharCode(e.which);
+            if (/[a-z\d]/i.test(charTyped)) {
+                this.termSearchs.push({
+                    letter: e.key
+                })
+            }
+            if(e.key == "Backspace"){
+                if(this.termSearchs.length > 0){
+                    this.termSearchs.pop()
+                } 
+            }
+            if(e.which == 32){
+                this.termSearchs.push({
+                    letter: " "
+                })
+            }
+        },
+        nothing(){
+            console.log("nothing")
+        }
+    },
+})
